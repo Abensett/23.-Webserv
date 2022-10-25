82eaa6f98f46b100ca238c4481d46a485a8c6bb9
Abensett <bensettiaimen@mailo.com>
2022-10-26 01:31:08 +0200

    asf

--- srcs/Webserver/srcs/main.c
+++ srcs/Webserver/srcs/main.c
@@ -4,25 +4,25 @@
 int main()
 {
   			FILE *html_file;
-				html_file = fopen("index.html", "r");
-				
-				if(html_file == NULL)
-    				return 1;
+	html_file = fopen("index.html", "r");
+	
+	if(html_file == NULL)
+    		return 1;
  
-				fseek(html_file, 0L, SEEK_END);
-				long numbytes;
-				char *buffer;
-				numbytes = ftell(html_file);
+	fseek(html_file, 0L, SEEK_END);
+	long numbytes;
+	char *buffer;
+	numbytes = ftell(html_file);
  
-				fseek(html_file, 0L, SEEK_SET);	
+	fseek(html_file, 0L, SEEK_SET);	
  
 
-				buffer = (char*)calloc(numbytes, sizeof(char));	
-				if(buffer == NULL)
-					return 1;
+	buffer = (char*)calloc(numbytes, sizeof(char));	
+	if(buffer == NULL)
+		return 1;
 
-				fread(buffer, sizeof(char), numbytes, html_file);
-				fclose(html_file);
+	fread(buffer, sizeof(char), numbytes, html_file);
+	fclose(html_file);
 				char	header[2048] = "Content-Type: text/html\n\n";
 				strcat(header, buffer);
 				printf("\n\n%s \n\n", header);
