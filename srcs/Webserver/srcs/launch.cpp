deec7ed5f4cebf2424eb28e1f4c269e6cc2d85ee
Abensett <bensettiaimen@mailo.com>
2022-10-26 01:22:04 +0200

    asf

--- srcs/Webserver/srcs/launch.cpp
+++ srcs/Webserver/srcs/launch.cpp
@@ -61,33 +61,37 @@ int		Webserver::launch(void)
 				recv(events[i].data.fd, request, 1024, 0);
 				std::cout << request << std::endl;
 
-				// FILE *html_file;
-				// html_file = fopen("index.html", "r");
-				
-				// if(html_file == NULL)
-    			// 	return 1;
- 
-				// fseek(html_file, 0L, SEEK_END);
-				// long numbytes;
-				// char *buffer;
-				// numbytes = ftell(html_file);
- 
-				// fseek(html_file, 0L, SEEK_SET);	
- 
 
-				// buffer = (char*)calloc(numbytes, sizeof(char));	
-				// if(buffer == NULL)
-				// 	return 1;
 
 				// fread(buffer, sizeof(char), numbytes, html_file);
 				// fclose(html_file);
 
 				// printf("\n\n%s \n\n", buffer);
 
-				char	header[2048] = "Content-Type: text/html\n\n";
-				// strcat(header, buffer);
+				FILE *html_file;
+				html_file = fopen("index.html", "r");
+				
+				if(html_file == NULL)
+    				return 1;
+ 
+				fseek(html_file, 0L, SEEK_END);
+				long numbytes;
+				char *buffer;
+				numbytes = ftell(html_file);
+ 
+				fseek(html_file, 0L, SEEK_SET);	
+ 
 
-				send(events[i].data.fd, "chat", strlen(header), 0);
+				buffer = (char*)calloc(numbytes, sizeof(char));	
+				if(buffer == NULL)
+					return 1;
+
+				fread(buffer, sizeof(char), numbytes, html_file);
+				fclose(html_file);
+				char	header[2048] = "Content-Type: text/html\n\n";
+				strcat(header, buffer);
+
+				send(events[i].data.fd,&header, strlen(header), 0);
 
 				if (remove_client(epoll_socket, client_socket, events))
 					return (1);
