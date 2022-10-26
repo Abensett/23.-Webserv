0322cd0334fbda26d1012078646d7298739dbd23
Abensett <bensettiaimen@mailo.com>
2022-10-26 10:22:23 +0200

    go

--- srcs/Webserver/srcs/launch.cpp
+++ srcs/Webserver/srcs/launch.cpp
@@ -7,20 +7,20 @@
 using std::cout;
 using std::endl;
 using std::string;
+using std::ifstream;
 const char *storedfile(std::string path)
 {
    std::ifstream fichier(path, std::ios::in);  // on ouvre en lecture
 
    if(fichier)
    {
-      //L'ouverture s'est bien passée, on peut donc lire
-
+      //L'ouverture s'est bien passée
       std::string ligne; //Une variable pour stocker les lignes lues
       std::string file_txt;
       file_txt =	"HTTP/1.0 200 OK\r\n\r\n";
-      while(getline(fichier, ligne)) //Tant qu'on n'est pas à la fin, on lit
+      while(getline(fichier, ligne)) //Tant pas à la fin, on lit
           file_txt = file_txt + ligne + '\n';
-      fichier.close();  // on ferme le fichier   
+      fichier.close();   
       char * cstr = new char [file_txt.length()+1];
       strcpy (cstr, file_txt.c_str());
       return cstr;
