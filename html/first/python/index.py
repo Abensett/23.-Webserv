948f74ac722a6c64e327cdff3cdd7a3ae6a3929c
Abensett <bensettiaimen@mailo.com>
2022-12-10 04:57:48 +0100

    cgi python

--- html/first/python/index.py
+++ html/first/python/index.py
@@ -10,7 +10,7 @@ print ("<head>")
 print ("<title>First CGI Program</title>")
 print ("</head>")
 print ("<body>")
-print ("<h2>Hello, %s is working in %s</h2>")
+print ("<h2>Hello, This HTML page was created from a python script !</h2>")
 
 print ("</body>")
 print ("</html>")
